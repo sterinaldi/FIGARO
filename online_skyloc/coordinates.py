4e44f689e2d94b57a7c16fd0d921155b077d4283
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-01-27 16:29:52 +0100

    made python

---
+++ online_skyloc/coordinates.py
@@ -0,0 +1,105 @@
+from __future__ import division
+import numpy as np
+
+def eq2ang(ra, dec):
+    """
+    convert equatorial ra,dec in radians to angular theta, phi in radians
+    parameters
+    ----------
+    ra: scalar or array
+        Right ascension in radians
+    dec: scalar or array
+        Declination in radians
+    returns
+    -------
+    theta,phi: tuple
+        theta = pi/2-dec*D2R # in [0,pi]
+        phi   = ra*D2R       # in [0,2*pi]
+    """
+    phi = ra
+    theta = np.pi/2. - dec
+    return theta, phi
+
+def ang2eq(theta, phi):
+    """
+    convert angular theta, phi in radians to equatorial ra,dec in radians
+    ra = phi*R2D            # [0,360]
+    dec = (pi/2-theta)*R2D  # [-90,90]
+    parameters
+    ----------
+    theta: scalar or array
+        angular theta in radians
+    phi: scalar or array
+        angular phi in radians
+    returns
+    -------
+    ra,dec: tuple
+        ra  = phi*R2D          # in [0,360]
+        dec = (pi/2-theta)*R2D # in [-90,90]
+    """
+    
+    ra = phi
+    dec = np.pi/2. - theta
+    return ra, dec
+
+def cartesian_to_spherical(vector):
+
+    """Convert the Cartesian vector [x, y, z] to spherical coordinates [r, theta, phi].
+
+    The parameter r is the radial distance, theta is the polar angle, and phi is the azimuth.
+
+
+    @param vector:  The Cartesian vector [x, y, z].
+    @type vector:   numpy rank-1, 3D array
+    @return:        The spherical coordinate vector [r, theta, phi].
+    @rtype:         numpy rank-1, 3D array
+    """
+    r = np.linalg.norm(vector)
+    unit = vector / r
+    theta = np.arccos(unit[2])
+    phi = np.arctan2(unit[1], unit[0])
+    coord = np.array([r,theta,phi])
+    return coord
+
+
+def spherical_to_cartesian(vector):
+    """Convert the spherical coordinate vector [r, theta, phi] to the Cartesian vector [x, y, z].
+
+    The parameter r is the radial distance, theta is the polar angle, and phi is the azimuth.
+
+
+    @param spherical_vect:  The spherical coordinate vector [r, theta, phi].
+    @type spherical_vect:   3D array or list
+    @param cart_vect:       The Cartesian vector [x, y, z].
+    @type cart_vect:        3D array or list
+    """
+    cart_vect = np.zeros(3)
+    # Trig alias.
+    sin_theta = np.sin(vector[1])
+    # The vector.
+    cart_vect[0] = vector[2] * np.cos(vector[0]) * sin_theta
+    cart_vect[1] = vector[2] * np.sin(vector[0]) * sin_theta
+    cart_vect[2] = vector[2] * np.cos(vector[1])
+
+    return cart_vect
+
+def celestial_to_cartesian(celestial_vect):
+    """Convert the spherical coordinate vector [r, dec, ra] to the Cartesian vector [x, y, z]."""
+    celestial_vect[1] = np.pi/2. - celestial_vect[1]
+    return spherical_to_cartesian(celestial_vect)
+
+def cartesian_to_celestial(cartesian_vect):
+    """Convert the Cartesian vector [x, y, z] to the celestial coordinate vector [r, dec, ra]."""
+    spherical_vect = cartesian_to_spherical(cartesian_vect)
+    spherical_vect[1] = np.pi/2. - spherical_vect[1]
+    coord = np.zeros(3)
+    coord[2] = spherical_vect[0]
+    coord[1] = spherical_vect[1]
+    coord[0] = spherical_vect[2]
+    return coord
+
+def Jacobian(cartesian_vect):
+    d = np.sqrt(cartesian_vect.dot(cartesian_vect))
+    d_sin_theta = np.sqrt(cartesian_vect[:-1].dot(cartesian_vect[:-1]))
+    return d*d_sin_theta
+
