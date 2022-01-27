4583ff5e8f0fb557d3077a1780b3afbb59ff7fc6
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-01-27 16:57:16 +0100

    maquillage

--- volrec.py
+++ volrec.py
@@ -6,12 +6,13 @@ from online_skyloc.incremental_gibbs import mixture
 from online_skyloc.coordinates import celestial_to_cartesian, cartesian_to_celestial
 
 samples = np.genfromtxt('data/GW150914_full_volume.txt')
+names = ['ra', 'dec', 'LD']
 
 cart_samples = celestial_to_cartesian(samples)
 bounds = np.array([[-1000,1000], [-1000,1000], [-1000,1000]])
 n_samps = len(samples)
 
-DPGMM = mixture(prior_pars, bounds)
+DPGMM = mixture(bounds)
 for s in tqdm(cart_samples):
     DPGMM.add_new_point(np.atleast_2d(s))
     
@@ -20,8 +21,8 @@ samps_mix = DPGMM.sample_from_dpgmm(n_samps)
 samps_mix = cartesian_to_celestial(samps_mix)
 samps_back = cartesian_to_celestial(cart_samples)
 
-c = corner(samps_back, color = 'orange', hist_kwargs={'density':True})
-c = corner(samps_mix, fig = c, color = 'blue', hist_kwargs={'density':True})
+c = corner(samps_back, color = 'orange', labels = names, hist_kwargs={'density':True})
+c = corner(samps_mix, fig = c, color = 'blue', labels = names, hist_kwargs={'density':True})
 
 plt.savefig('data/GW150914_full_volume.pdf', bbox_inches = 'tight')
 
