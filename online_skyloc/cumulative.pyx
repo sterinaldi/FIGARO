b6a2bccfed96e00dd09945092e4f2a99c4e12749
Walter Del Pozzo <walter.delpozzo@ligo.org>
2022-01-31 17:17:23 +0100

    modified typing to double

--- online_skyloc/cumulative.pyx
+++ online_skyloc/cumulative.pyx
@@ -10,22 +10,17 @@ from libc.math cimport log,exp
 '''
 From https://github.com/wdpozzo/3d_volume/cumulative.pyx
 '''
-#
-#cdef extern from "math.h":
-#    double log(double x)
-#cdef extern from "math.h":
-#    double exp(double x)
 
-cdef inline double d_max(float a, float b) nogil: return a if a >= b else b
-cdef inline double log_add(float x, float y) nogil: return x+log(1+exp(y-x)) if x >= y else y+log(1+exp(x-y))
+cdef inline double d_max(double a, double b) nogil: return a if a >= b else b
+cdef inline double log_add(double x, double y) nogil: return x+log(1+exp(y-x)) if x >= y else y+log(1+exp(x-y))
 
-def fast_log_cumulative(np.ndarray[float, ndim=1]  f):
+def fast_log_cumulative(np.ndarray[double, ndim=1, mode="c"]  f):
     return _fast_log_cumulative(f)
 
-cdef np.ndarray[float, ndim=1] _fast_log_cumulative(np.ndarray[float, ndim=1]  f):
+cdef np.ndarray[double, ndim=1, mode="c"] _fast_log_cumulative(np.ndarray[double, ndim=1, mode="c"] f):
 
     cdef unsigned int n = f.shape[0]
-    cdef np.ndarray[float, ndim=1] h = np.zeros(n, dtype = np.float64)
+    cdef np.ndarray[double, ndim=1, mode="c"] h = np.zeros(n, dtype = np.double)
 
     h[0] = f[0]
     for i in range(1,n):
