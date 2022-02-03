7b975cbbdd271c0ff1f5d61474f33b95cc136119
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-02-03 17:11:45 +0100

    removed volumereconstruction

--- online_skyloc/incremental_gibbs.py
+++ online_skyloc/incremental_gibbs.py
@@ -1,26 +1,14 @@
 import numpy as np
-from itertools import product
 from collections import Counter
 
 from scipy.special import gammaln, logsumexp
 from scipy.stats import multivariate_normal as mn
 
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-import matplotlib.patches as mpatches
-from matplotlib.lines import Line2D
-from corner import corner
-
+from pathlib import Path
 import dill
 
 from online_skyloc.decorators import *
 from online_skyloc.transform import *
-from online_skyloc.coordinates import celestial_to_cartesian, cartesian_to_celestial, inv_Jacobian
-from online_skyloc.credible_regions import ConfidenceArea
-
-from pathlib import Path
-from distutils.spawn import find_executable
-from tqdm import tqdm
 
 from numba import jit, njit
 from numba.extending import get_cython_function_address
@@ -36,16 +24,7 @@ gammaln_float64 = functype(addr)
 
 @njit
 def numba_gammaln(x):
-  return gammaln_float64(x)
-
-rcParams["xtick.labelsize"]=14
-rcParams["ytick.labelsize"]=14
-rcParams["xtick.direction"]="in"
-rcParams["ytick.direction"]="in"
-rcParams["legend.fontsize"]=15
-rcParams["axes.labelsize"]=16
-rcParams["axes.grid"] = True
-rcParams["grid.alpha"] = 0.6
+    return gammaln_float64(x)
 
 class component:
     def __init__(self, x, prior):
@@ -126,6 +105,7 @@ class mixture:
                        prior_pars = None,
                        alpha0     = 1.,
                        sigma_max  = 0.05,
+                       out_folder = '.'
                        ):
         self.bounds   = np.array(bounds)
         self.dim      = len(self.bounds)
@@ -242,126 +222,6 @@ class mixture:
         p = logsumexp(np.array([w + mn(comp.mu, comp.sigma).logpdf(x) for comp, w in zip(self.mixture, self.log_w)]), axis = 0)
         return p + probit_logJ(x, self.bounds)
 
-class VolumeReconstruction(mixture):
-    def __init__(self, max_dist,
-                       out_folder   = '.',
-                       prior_pars   = None,
-                       alpha0       = 1,
-                       sigma_max    = 0.05,
-                       n_gridpoints = [720, 360, 100], # RA, dec, DL
-                       name         = 'skymap',
-                       labels       = ['$\\alpha$', '$\\delta$', '$D\ [Mpc]$'],
-                       levels       = [0.50, 0.90],
-                       latex        = False,
-                       incr_plot    = False,
-                       ):
-        
-        self.max_dist = max_dist
-        bounds = np.array([[-max_dist, max_dist] for _ in range(3)])
-        super().__init__(bounds, prior_pars, alpha0, sigma_max)
-        
-        if incr_plot:
-            self.next_plot = 20
-        else:
-            self.next_plot = np.inf
-
-        if latex:
-            if find_executable('latex'):
-                rcParams["text.usetex"] = True
-        
-        # Grid
-        self.ra   = np.linspace(0,2*np.pi, n_gridpoints[0])
-        self.dec  = np.linspace(-np.pi/2*0.99, np.pi/2.*0.99, n_gridpoints[1])
-        self.dist = np.linspace(max_dist*0.01, max_dist*0.99, n_gridpoints[2])
-        
-        self.dD   = np.diff(self.dist)[0]
-        self.dra  = np.diff(self.ra)[0]
-        self.ddec = np.diff(self.dec)[0]
-        
-        self.grid = np.array([v for v in product(*(self.ra,self.dec,self.dist))])
-        self.grid2d = np.array([v for v in product(*(self.ra,self.dec))])
-        self.ra_2d, self.dec_2d = np.meshgrid(self.ra, self.dec)
-        
-        self.cartesian_grid = celestial_to_cartesian(self.grid)
-        self.probit_grid = transform_to_probit(self.cartesian_grid, self.bounds)
-
-        self.log_inv_J = -np.log(inv_Jacobian(self.grid)).reshape(len(self.ra), len(self.dec), len(self.dist)) + probit_logJ(self.probit_grid, self.bounds).reshape(len(self.ra), len(self.dec), len(self.dist))
-        self.inv_J = np.exp(self.log_inv_J)
-        
-        self.levels = levels
-        
-        self.out_folder = Path(out_folder).resolve()
-        self.skymap_folder = Path(out_folder, 'skymaps', name)
-        if not self.skymap_folder.exists():
-            self.skymap_folder.mkdir()
-        self.density_folder = Path(out_folder, 'density')
-        if not self.density_folder.exists():
-            self.density_folder.mkdir()
-        self.name = name
-        self.labels = labels
-        
-    def add_sample(self, x):
-        cart_x = celestial_to_cartesian(x)
-        self.add_new_point(cart_x)
-        if self.n_pts == self.next_plot:
-            self.next_plot = 2*self.next_plot
-            self.make_skymap()
-    
-    def sample_from_volume(self, n_samps):
-        samples = self.sample_from_dpgmm(n_samps)
-        return cartesian_to_celestial(samples)
-    
-    def plot_samples(self, n_samps, initial_samples = None):
-        mix_samples = self.sample_from_volume(n_samps)
-        if initial_samples is not None:
-            c = corner(initial_samples, color = 'coral', labels = self.labels, hist_kwargs={'density':True, 'label':'$\mathrm{Samples}$'})
-            c = corner(mix_samples, fig = c, color = 'dodgerblue', labels = self.labels, hist_kwargs={'density':True, 'label':'$\mathrm{DPGMM}$'})
-        else:
-            c = corner(mix_samples, fig = c, color = 'dodgerblue', labels = self.labels, hist_kwargs={'density':True, 'label':'$\mathrm{DPGMM}$'})
-        plt.legend(loc = 0, frameon = False,fontsize = 15, bbox_to_anchor = (1-0.05, 2.8))
-        plt.savefig(Path(self.skymap_folder, 'samples_'+self.name+'.pdf'), bbox_inches = 'tight')
-        plt.close()
-    
-    def evaluate_skymap(self):
-
-        p_vol     = self._evaluate_mixture_in_probit(self.probit_grid).reshape(len(self.ra), len(self.dec), len(self.dist)) * self.inv_J
-        norm_p_vol = (p_vol*self.dD*self.dra*self.ddec).sum()
-        p_vol      = p_vol/norm_p_vol
-        self.p_skymap = (p_vol*self.dD).sum(axis = -1)
-        self.log_p_skymap = np.log(self.p_skymap)
-        self.areas, self.idx_CR, self.heights = ConfidenceArea(self.log_p_skymap, self.dec, self.ra, adLevels = self.levels)
-            
-    def make_skymap(self, final_map = False):
-        self.evaluate_skymap()
-        fig = plt.figure()
-        ax = fig.add_subplot(111)#, projection='mollweide')
-        c = ax.contourf(self.ra_2d, self.dec_2d, self.p_skymap.T, 990, cmap = 'Reds')
-        c1 = ax.contour(self.ra_2d, self.dec_2d, self.log_p_skymap.T, np.sort(self.heights), colors = 'black', linewidths = 0.5, linestyles = 'dashed')
-        ax.clabel(c1, fmt = {l:'{0:.0f}%'.format(100*s) for l,s in zip(c1.levels, self.levels[::-1])}, fontsize = 5)
-        for i in range(len(self.areas)):
-            c1.collections[i].set_label('${0:.0f}\\%'.format(100*self.levels[-i])+ '\ \mathrm{CR}:'+'{0:.1f}'.format(self.areas[-i]) + '\ \mathrm{deg}^2$')
-        handles, labels = ax.get_legend_handles_labels()
-        patch = mpatches.Patch(color='grey', label='$N_{\mathrm{samples}} =' +'{0}$'.format(self.n_pts), alpha = 0)
-        handles.append(patch)
-        ax.set_xlabel('$\\alpha$')
-        ax.set_ylabel('$\\delta$')
-        ax.legend(handles = handles, loc = 0, frameon = False, fontsize = 10, handlelength=0, handletextpad=0, markerscale=0)
-#        plt.colorbar(c, label = '$p(\\alpha,\\delta)$', orientation='horizontal')
-        if final_map:
-            fig.savefig(Path(self.skymap_folder, self.name+'_all.pdf'), bbox_inches = 'tight')
-        else:
-            fig.savefig(Path(self.skymap_folder, self.name+'_{0}'.format(self.n_pts)+'.pdf'), bbox_inches = 'tight')
-        plt.close()
-    
     def save_density(self):
-        with open(Path(self.density_folder, self.name + '_density.pkl'), 'wb') as dill_file:
+        with open(Path(self.out_folder, 'mixture.pkl'), 'wb') as dill_file:
             dill.dump(self, dill_file)
-
-    def density_from_samples(self, samples):
-        n_samps = len(samples)
-        samples_copy = np.copy(samples)
-        for s in tqdm(samples_copy):
-            self.add_sample(s)
-        self.plot_samples(n_samps, initial_samples = samples)
-        self.make_skymap(final_map = True)
-        self.save_density()
