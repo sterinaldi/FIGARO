294a33790a2e4c755f934a3b50a5b875168d276c
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-02-04 12:26:58 +0100

    add gif and checks

--- online_skyloc/threeDvolume.py
+++ online_skyloc/threeDvolume.py
@@ -1,12 +1,14 @@
 import numpy as np
 from itertools import product
 import h5py
+import re
 
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import matplotlib.patches as mpatches
 from matplotlib.lines import Line2D
 from corner import corner
+import imageio
 
 import dill
 
@@ -29,6 +31,20 @@ rcParams["axes.labelsize"]=16
 rcParams["axes.grid"] = True
 rcParams["grid.alpha"] = 0.6
 
+# natural sorting.
+# list.sort(key = natural_keys)
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split(r'(\d+)', text) ]
+
 class VolumeReconstruction(mixture):
     def __init__(self, max_dist,
                        out_folder   = '.',
@@ -44,6 +60,7 @@ class VolumeReconstruction(mixture):
                        glade_file   = None,
                        cosmology    = {'h': 0.674, 'om': 0.315, 'ol': 0.685},
                        n_gal_to_print = 100,
+                       region_to_plot = 0.5
                        ):
                 
         self.max_dist = max_dist
@@ -75,46 +92,33 @@ class VolumeReconstruction(mixture):
         self.probit_grid = transform_to_probit(self.cartesian_grid, self.bounds)
 
         # Jacobians
-        self.log_inv_J = -np.log(inv_Jacobian(self.grid)).reshape(len(self.ra), len(self.dec), len(self.dist)) + probit_logJ(self.probit_grid, self.bounds).reshape(len(self.ra), len(self.dec), len(self.dist))
+        self.log_inv_J = -np.log(inv_Jacobian(self.grid)) - probit_logJ(self.probit_grid, self.bounds)
         self.inv_J = np.exp(self.log_inv_J)
         
         # Credible regions levels
-        self.levels = levels
+        self.levels = np.array(levels)
         
         # Output
+        self.name       = name
+        self.labels     = labels
         self.out_folder = Path(out_folder).resolve()
-        if not Path(out_folder, 'skymaps').exists():
-            Path(out_folder, 'skymaps').mkdir()
-        if not Path(out_folder, 'volume').exists():
-            Path(out_folder, 'volume').mkdir()
-        if not Path(out_folder, 'catalogs').exists():
-            Path(out_folder, 'catalogs').mkdir()
-        self.skymap_folder = Path(out_folder, 'skymaps', name)
-        if not self.skymap_folder.exists():
-            self.skymap_folder.mkdir()
-        self.volume_folder = Path(out_folder, 'volume', name)
-        if not self.volume_folder.exists():
-            self.volume_folder.mkdir()
-        self.catalog_folder = Path(out_folder, 'catalogs', name)
-        if not self.catalog_folder.exists():
-            self.catalog_folder.mkdir()
-        self.density_folder = Path(out_folder, 'density')
-        if not self.density_folder.exists():
-            self.density_folder.mkdir()
-        self.name = name
-        self.labels = labels
+        self.make_folders()
         
         # Catalog
         self.cosmology = CosmologicalParameters(cosmology['h'], cosmology['om'], cosmology['ol'], 1, 0)
         self.catalog   = None
-        if glade_file is not None:
+        if glade_file is not None and self.max_dist < 3000:
             self.catalog = self.load_glade(glade_file)
             self.cartesian_catalog = celestial_to_cartesian(self.catalog)
             self.probit_catalog    = transform_to_probit(self.cartesian_catalog, self.bounds)
-            self.log_inv_J_cat     = -np.log(inv_Jacobian(self.catalog)) + probit_logJ(self.probit_catalog, self.bounds)
+            self.log_inv_J_cat     = -np.log(inv_Jacobian(self.catalog)) - probit_logJ(self.probit_catalog, self.bounds)
             self.inv_J_cat         = np.exp(self.log_inv_J)
         self.n_gal_to_print = n_gal_to_print
-
+        if region_to_plot in self.levels:
+            self.region = region_to_plot
+        else:
+            self.region = self.levels[0]
+    
     def load_glade(self, glade_file):
         with h5py.File(glade_file, 'r') as f:
             ra  = np.array(f['ra'])
@@ -126,6 +130,30 @@ class VolumeReconstruction(mixture):
         catalog = catalog[catalog[:,2] < self.max_dist]
         return catalog
 
+    def make_folders(self):
+        if not Path(self.out_folder, 'skymaps').exists():
+            Path(self.out_folder, 'skymaps').mkdir()
+        if not Path(self.out_folder, 'volume').exists():
+            Path(self.out_folder, 'volume').mkdir()
+        if not Path(self.out_folder, 'catalogs').exists():
+            Path(self.out_folder, 'catalogs').mkdir()
+        self.skymap_folder = Path(self.out_folder, 'skymaps', self.name)
+        if not self.skymap_folder.exists():
+            self.skymap_folder.mkdir()
+        self.volume_folder = Path(self.out_folder, 'volume', self.name)
+        if not self.volume_folder.exists():
+            self.volume_folder.mkdir()
+        self.catalog_folder = Path(self.out_folder, 'catalogs', self.name)
+        if not self.catalog_folder.exists():
+            self.catalog_folder.mkdir()
+        self.density_folder = Path(self.out_folder, 'density')
+        if not self.density_folder.exists():
+            self.density_folder.mkdir()
+        self.gif_folder = Path(self.out_folder, 'gif')
+        if not self.gif_folder.exists():
+            self.gif_folder.mkdir()
+
+
     def add_sample(self, x):
         self.volume_already_evaluated = False
         cart_x = celestial_to_cartesian(x)
@@ -133,7 +161,8 @@ class VolumeReconstruction(mixture):
         if self.n_pts == self.next_plot:
             self.next_plot = 2*self.next_plot
             self.make_skymap()
-            self.make_volume_map()
+            if self.catalog is not None:
+                self.make_volume_map()
     
     def sample_from_volume(self, n_samps):
         samples = self.sample_from_dpgmm(n_samps)
@@ -152,32 +181,59 @@ class VolumeReconstruction(mixture):
     
     def evaluate_skymap(self):
         if not self.volume_already_evaluated:
-            p_vol               = self._evaluate_mixture_in_probit(self.probit_grid).reshape(len(self.ra), len(self.dec), len(self.dist)) * self.inv_J
+            p_vol               = self._evaluate_mixture_in_probit(self.probit_grid) * self.inv_J
             self.norm_p_vol     = (p_vol*self.dD*self.dra*self.ddec).sum()
             self.log_norm_p_vol = np.log(self.norm_p_vol)
             self.p_vol          = p_vol/self.norm_p_vol
-            self.log_p_vol      = np.log(self.p_vol)
+            
+            # By default computes log(p_vol). If -infs are present, computes log_p_vol
+            with np.errstate(divide='raise'):
+                try:
+                    self.log_p_vol = np.log(self.p_vol)
+                except FloatingPointError:
+                    self.log_p_vol = self._evaluate_log_mixture_in_probit(self.probit_grid) + self.log_inv_J - self.log_norm_p_vol
+                    
+            self.p_vol     = self.p_vol.reshape(len(self.ra), len(self.dec), len(self.dist))
+            self.log_p_vol = self.log_p_vol.reshape(len(self.ra), len(self.dec), len(self.dist))
             self.volume_already_evaluated = True
-        self.p_skymap = (p_vol*self.dD).sum(axis = -1)
-        self.log_p_skymap = np.log(self.p_skymap)
+
+        self.p_skymap = (self.p_vol*self.dD).sum(axis = -1)
+        
+        # By default computes log(p_skymap). If -infs are present, computes log_p_skymap
+        with np.errstate(divide='raise'):
+            try:
+                self.log_p_skymap = np.log(self.p_skymap)
+            except FloatingPointError:
+                self.log_p_skymap = logsumexp(self.log_p_vol + np.log(dD), axis = -1)
+
         self.areas, self.skymap_idx_CR, self.skymap_heights = ConfidenceArea(self.log_p_skymap, self.ra, self.dec, adLevels = self.levels)
     
     def evaluate_volume_map(self):
         if not self.volume_already_evaluated:
-            p_vol               = self._evaluate_mixture_in_probit(self.probit_grid).reshape(len(self.ra), len(self.dec), len(self.dist)) * self.inv_J
+            p_vol               = self._evaluate_mixture_in_probit(self.probit_grid) * self.inv_J
             self.norm_p_vol     = (p_vol*self.dD*self.dra*self.ddec).sum()
             self.log_norm_p_vol = np.log(self.norm_p_vol)
             self.p_vol          = p_vol/self.norm_p_vol
-            self.log_p_vol      = np.log(self.p_vol)
+            
+            # By default, just uses log(p_vol). If -infs are present, computes log_p_vol
+            with np.errstate(divide='raise'):
+                try:
+                    self.log_p_vol = np.log(self.p_vol)
+                except FloatingPointError:
+                    self.log_p_vol = self._evaluate_log_mixture_in_probit(self.probit_grid) + self.log_inv_J - self.log_norm_p_vol
+
+            self.p_vol     = self.p_vol.reshape(len(self.ra), len(self.dec), len(self.dist))
+            self.log_p_vol = self.log_p_vol.reshape(len(self.ra), len(self.dec), len(self.dist))
             self.volume_already_evaluated = True
+            
         self.volumes, self.idx_CR, self.volume_heights = ConfidenceVolume(self.log_p_vol, self.ra, self.dec, self.dist, adLevels = self.levels)
         
     def evaluate_catalog(self):
-        log_p_cat          = self._evaluate_log_mixture_in_probit(self.probit_catalog) + self.log_inv_J_cat - self.log_norm_p_vol
-        self.log_p_cat_to_plot = log_p_cat[np.where(log_p_cat > self.volume_heights[0])]
-        self.p_cat_to_plot     = np.exp(self.log_p_cat_to_plot)
-        self.cat_to_plot_celestial = self.catalog[np.where(log_p_cat > self.volume_heights[0])]
-        self.cat_to_plot_cartesian = self.cartesian_catalog[np.where(log_p_cat > self.volume_heights[0])]
+        log_p_cat                  = self._evaluate_log_mixture_in_probit(self.probit_catalog) + self.log_inv_J_cat - self.log_norm_p_vol
+        self.log_p_cat_to_plot     = log_p_cat[np.where(log_p_cat > self.volume_heights[np.where(self.levels == self.region)])]
+        self.p_cat_to_plot         = np.exp(self.log_p_cat_to_plot)
+        self.cat_to_plot_celestial = self.catalog[np.where(log_p_cat > self.volume_heights[np.where(self.levels == self.region)])]
+        self.cat_to_plot_cartesian = self.cartesian_catalog[np.where(log_p_cat > self.volume_heights[np.where(self.levels == self.region)])]
         
         sorted_cat = np.c_[self.cat_to_plot_celestial[np.argsort(self.log_p_cat_to_plot)], np.sort(self.log_p_cat_to_plot)][::-1]
         np.savetxt(Path(self.catalog_folder, self.name+'_{0}'.format(self.n_pts)+'.txt'), sorted_cat, header = 'ra dec dist logp')
@@ -199,8 +255,10 @@ class VolumeReconstruction(mixture):
         ax.legend(handles = handles, loc = 0, frameon = False, fontsize = 10, handlelength=0, handletextpad=0, markerscale=0)
         if final_map:
             fig.savefig(Path(self.skymap_folder, self.name+'_all.pdf'), bbox_inches = 'tight')
+            fig.savefig(Path(self.gif_folder, self.name+'_all.png'), bbox_inches = 'tight')
         else:
             fig.savefig(Path(self.skymap_folder, self.name+'_{0}'.format(self.n_pts)+'.pdf'), bbox_inches = 'tight')
+            fig.savefig(Path(self.gif_folder, self.name+'_{0}'.format(self.n_pts)+'.png'), bbox_inches = 'tight')
         plt.close()
     
     def make_volume_map(self, final_map = False):
@@ -210,9 +268,9 @@ class VolumeReconstruction(mixture):
         # Cartesian plot
         fig = plt.figure()
         ax = fig.add_subplot(111, projection = '3d')
-        ax.scatter(self.cat_to_plot_cartesian[:,0], self.cat_to_plot_cartesian[:,1], self.cat_to_plot_cartesian[:,2], c = self.p_cat_to_plot, marker = '.', alpha = 0.5, s = 0.5)
+        ax.scatter(self.cat_to_plot_cartesian[:,0], self.cat_to_plot_cartesian[:,1], self.cat_to_plot_cartesian[:,2], c = self.p_cat_to_plot, marker = '.', alpha = 0.7, s = 0.5, cmap = 'Reds')
         vol_str = ['${0:.0f}\\%'.format(100*self.levels[-i])+ '\ \mathrm{CR}:'+'{0:.0f}'.format(self.volumes[-i]) + '\ \mathrm{Mpc}^3$' for i in range(len(self.volumes))]
-        vol_str = '\n'.join(vol_str + ['$N_{\mathrm{gal}}\ \mathrm{in}\ '+'{0:.0f}\\%'.format(100*self.levels[0])+ '\ \mathrm{CR}:'+'{0}$'.format(len(self.cat_to_plot_cartesian))])
+        vol_str = '\n'.join(vol_str + ['$N_{\mathrm{gal}}\ \mathrm{in}\ '+'{0:.0f}\\%'.format(100*self.levels[np.where(self.levels == self.region)][0])+ '\ \mathrm{CR}:'+'{0}$'.format(len(self.cat_to_plot_cartesian))])
         ax.text2D(0.05, 0.95, vol_str, transform=ax.transAxes)
         ax.set_xlabel('$x$')
         ax.set_ylabel('$y$')
@@ -226,9 +284,9 @@ class VolumeReconstruction(mixture):
         # Celestial plot
         fig = plt.figure()
         ax = fig.add_subplot(111, projection = '3d')
-        ax.scatter(self.cat_to_plot_celestial[:,0], self.cat_to_plot_celestial[:,1], self.cat_to_plot_celestial[:,2], c = self.p_cat_to_plot, marker = '.', alpha = 0.5, s = 0.5)
+        ax.scatter(self.cat_to_plot_celestial[:,0], self.cat_to_plot_celestial[:,1], self.cat_to_plot_celestial[:,2], c = self.p_cat_to_plot, marker = '.', alpha = 0.7, s = 0.5, cmap = 'Reds')
         vol_str = ['${0:.0f}\\%'.format(100*self.levels[-i])+ '\ \mathrm{CR}:'+'{0:.0f}'.format(self.volumes[-i]) + '\ \mathrm{Mpc}^3$' for i in range(len(self.volumes))]
-        vol_str = '\n'.join(vol_str + ['$N_{\mathrm{gal}}\ \mathrm{in}\ '+'{0:.0f}\\%'.format(100*self.levels[0])+ '\ \mathrm{CR}:'+'{0}$'.format(len(self.cat_to_plot_celestial))])
+        vol_str = '\n'.join(vol_str + ['$N_{\mathrm{gal}}\ \mathrm{in}\ '+'{0:.0f}\\%'.format(100*self.levels[np.where(self.levels == self.region)][0])+ '\ \mathrm{CR}:'+'{0}$'.format(len(self.cat_to_plot_celestial))])
         ax.text2D(0.05, 0.95, vol_str, transform=ax.transAxes)
         ax.set_xlabel('$\\alpha$')
         ax.set_ylabel('$\\delta$')
@@ -238,6 +296,16 @@ class VolumeReconstruction(mixture):
         else:
             fig.savefig(Path(self.volume_folder, self.name+'_{0}'.format(self.n_pts)+'.pdf'), bbox_inches = 'tight')
         plt.close()
+    
+    def make_gif(self):
+        files = [f for f in self.gif_folder.glob(self.name + '*' + '.png')]
+        path_files = [str(f) for f in files]
+        path_files.sort(key = natural_keys)
+        images = []
+        for file in path_files:
+            images.append(imageio.imread(file))
+        imageio.mimsave(Path(self.gif_folder, self.name + '.gif'), images, fps = 1)
+        [f.unlink() for f in files]
     
     def save_density(self):
         with open(Path(self.density_folder, self.name + '_density.pkl'), 'wb') as dill_file:
@@ -252,5 +320,6 @@ class VolumeReconstruction(mixture):
         self.make_skymap(final_map = True)
         if self.catalog is not None:
             self.make_volume_map(final_map = True)
+        self.make_gif()
         self.save_density()
 
