1e0ffeb3a0dda74fc62739d4aa31a8112c3c4b7e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-02-06 21:43:57 +0100

    add invgamma prior on sigma 1d

--- figaro/integral.pyx
+++ figaro/integral.pyx
@@ -5,11 +5,13 @@ from libc.math cimport log, sqrt, M_PI, exp, HUGE_VAL, atan2, acos, sin, cos
 cimport cython
 from numpy.linalg import det, inv
 from scipy.stats import multivariate_normal as mn
+from scipy.stats import invgamma
+from scipy.special import gammaln
 
 cdef double LOGSQRT2 = log(sqrt(2*M_PI))
 
 cdef inline double log_add(double x, double y) nogil: return x+log(1.0+exp(y-x)) if x >= y else y+log(1.0+exp(x-y))
-cdef inline double _scalar_log_norm(double x, double x0, double s) nogil: return -(x-x0)*(x-x0)/(2*s*s) - LOGSQRT2 - 0.5*log(s)
+cdef inline double _scalar_log_norm(double x, double x0, double s): return -(x-x0)*(x-x0)/(2*s*s) - LOGSQRT2 - 0.5*log(s)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -72,3 +74,44 @@ cdef double _integrand(np.ndarray[double, ndim = 1, mode = "c"] mean, np.ndarray
 def integrand(np.ndarray[double, ndim = 1, mode = "c"] mean, np.ndarray[double, ndim = 2, mode = "c"] covariance, list events, unsigned int dim):
     return _integrand(mean, covariance, events, dim)
 
+######
+# 1D #
+######
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef double _log_prob_component_1d(double mu, double mean, double sigma, double w):
+    return log(w) + _scalar_log_norm(mu, mean, sigma)
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef double _log_prob_mixture_1d(double mu, double var, object ev):
+    cdef double logP = -HUGE_VAL
+    for comp_mean, comp_weight in zip(ev.means, ev.w):
+        logP = log_add(logP, _log_prob_component_1d(mu, comp_mean[0], var, comp_weight))
+    return logP
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+cdef double _integrand_1d(double mean, double var, list events, double a, double b):
+    cdef unsigned int i,j
+    cdef double logprob = 0.0
+    cdef object ev
+    for ev in events:
+        logprob += _log_prob_mixture_1d(mean, var, ev)
+    return logprob + _log_invgamma(var, a, b**2) - np.log(40) #FIXME: finire inv gamma
+    
+def integrand_1d(double var, double mean, list events, double a, double b):
+    return np.exp(_integrand_1d(mean, var, events, a, b))
+
+def log_integrand_1d(double mean, double var, list events, double a, double b):
+    return _integrand_1d(mean, var, events, a, b)
+
+cdef double _log_invgamma(double var, double a, double b):
+    return a*np.log(b) - (a+1)*np.log(var**2) - b/var**2 - gammaln(a)
