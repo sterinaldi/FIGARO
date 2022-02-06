7d2e7932248bfde59b192015cc696e3940168cae
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-02-06 17:28:39 +0100

    added numpy array definitions

--- figaro/integral.pyx
+++ figaro/integral.pyx
@@ -11,25 +11,11 @@ cdef double LOGSQRT2 = log(sqrt(2*M_PI))
 cdef inline double log_add(double x, double y) nogil: return x+log(1.0+exp(y-x)) if x >= y else y+log(1.0+exp(x-y))
 cdef inline double _scalar_log_norm(double x, double x0, double s) nogil: return -(x-x0)*(x-x0)/(2*s*s) - LOGSQRT2 - 0.5*log(s)
 
-cdef class mixture_cython:
-    def __cinit__(self, np.ndarray[double, ndim = 2, mode = "c"] means,
-                        np.ndarray[double, ndim = 3, mode = "c"] covs,
-                        np.ndarray[double, ndim = 1, mode = "c"] w,
-                        np.ndarray[double, ndim = 2, mode = "c"] bounds,
-                        ):
-        self.means  = means
-        self.covs   = covs
-        self.w      = w
-        self.log_w  = np.log(w)
-        self.bounds = bounds
-    def __reduce__(self):
-        return (mixture_cython, (self.means, self.covs, self.w, self.bounds))
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef double _triple_product(double[:] x, double[:] mu, double[:,:] inv_cov) nogil:
+cdef double _triple_product(np.ndarray[double, ndim = 1, mode = "c"] x, np.ndarray[double, ndim = 1, mode = "c"] mu, np.ndarray[double, ndim = 2, mode = "c"] inv_cov):
     cdef unsigned int i,j
     cdef unsigned int n = x.shape[0]
     cdef double res     = 0.0
@@ -42,8 +28,8 @@ cdef double _triple_product(double[:] x, double[:] mu, double[:,:] inv_cov) nogi
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef double _log_norm(double[:] x, double[:] mu, double[:,:] cov):
-    cdef double[:,:] inv_cov = np.linalg.inv(cov)
+cdef double _log_norm(np.ndarray[double, ndim = 1, mode = "c"] x, np.ndarray[double, ndim = 1, mode = "c"] mu, np.ndarray[double, ndim = 2, mode = "c"] cov):
+    cdef np.ndarray inv_cov  = np.linalg.inv(cov)
     cdef double exponent     = -0.5*_triple_product(x, mu, inv_cov)
     cdef double lognorm      = LOGSQRT2-0.5*np.linalg.slogdet(inv_cov)[1]
     return -lognorm+exponent
@@ -58,14 +44,14 @@ def scalar_log_norm(double x, double x0, double s):
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef inline double _log_prob_component(np.ndarray mu, np.ndarray mean, np.ndarray sigma, double w):
+cdef double _log_prob_component(np.ndarray[double, ndim = 1, mode = "c"] mu, np.ndarray[double, ndim = 1, mode = "c"] mean, np.ndarray[double, ndim = 2, mode = "c"] sigma, double w):
     return log(w) + _log_norm(mu, mean, sigma)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef double _log_prob_mixture(np.ndarray mu, np.ndarray sigma, dict ev):
+cdef double _log_prob_mixture(np.ndarray[double, ndim = 1, mode = "c"] mu, np.ndarray[double, ndim = 2, mode = "c"] sigma, object ev):
     cdef double logP = -HUGE_VAL
     for comp_mean, comp_weight in zip(ev.means, ev.w):
         logP = log_add(logP, _log_prob_component(mu, comp_mean, sigma, comp_weight))
@@ -75,14 +61,14 @@ cdef double _log_prob_mixture(np.ndarray mu, np.ndarray sigma, dict ev):
 @cython.wraparound(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
-cdef double _integrand(double[:] mean, double[:,:] covariance, list events, unsigned int dim):
+cdef double _integrand(np.ndarray[double, ndim = 1, mode = "c"] mean, np.ndarray[double, ndim = 2, mode = "c"] covariance, list events, unsigned int dim):
     cdef unsigned int i,j
     cdef double logprob = 0.0
-    cdef dict ev
+    cdef object ev
     for ev in events:
         logprob += _log_prob_mixture(mean, covariance, ev)
     return logprob
 
-def integrand(double[:] mean, double[:,:] covariance, list events, unsigned int dim):
+def integrand(np.ndarray[double, ndim = 1, mode = "c"] mean, np.ndarray[double, ndim = 2, mode = "c"] covariance, list events, unsigned int dim):
     return _integrand(mean, covariance, events, dim)
 
