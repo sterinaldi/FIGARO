a3edf17cdfa8482bbe2500ffe4f5590f8517696a
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-02-12 14:21:04 +0100

    added Integrator class

--- figaro/metropolis.py
+++ figaro/metropolis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from numpy.random import uniform
+import cpnest.model
 from numba import jit, njit, prange
 from numba.extending import get_cython_function_address
 import ctypes
@@ -48,7 +48,7 @@ def sample_point(means, sigmas, m_min = -20, m_max = 20, s_min = 0, s_max = 1, b
 def log_integrand_1d(mu, sigma, means, sigmas, a, b):
     logP = -np.inf
     for i in prange(len(means)):
-        logP = log_add(logP, log_norm(means[i][0], mu, np.sqrt(sigmas[i] + sigma**2)))
+        logP = log_add(logP, log_norm_1d(means[i][0], mu, np.sqrt(sigmas[i] + sigma**2)))
     return logP + log_invgamma(sigma, a, b)
 
 @jit
@@ -56,5 +56,69 @@ def log_invgamma(var, a, b):
     return a*np.log(b) - (a+1)*np.log(var**2) - b/var**2 - numba_gammaln(a)
 
 @jit
-def log_norm(x, m, s):
+def log_norm_1d(x, m, s):
     return -(x-m)**2/(2*s*s) - 0.5*np.log(2*np.pi) - np.log(s)
+
+class Integrator(cpnest.model.Model):
+    
+    def __init__(self, means, covs, dim, df, L):
+        super(Integrator, self).__init__()
+        self.means     = means
+        self.covs      = covs
+        self.dim       = dim
+        self.names     = ['m{0}'.format(i+1) for i in range(self.dim)] + ['s{0}'.format(i+1) for i in range(self.dim)] + ['r{0}'.format(j) for j in range(int(self.dim*(self.dim-1)/2.))]
+        self.bounds    = [[-20, 20] for _ in range(self.dim)] + [[0, 1] for _ in range(self.dim)] + [[-1,1] for _ in range(int(self.dim*(self.dim-1)/2.))]
+        self.prior     = invwishart(df = df, scale = L)
+        self.mu_prior  = -self.dim*np.log(40)
+    
+    def log_prior(self, x):
+        logP = super(Integrator, self).log_prior(x)
+        if not np.isfinite(logP):
+            return -np.inf
+        self.mean, self.cov_mat = build_mean_cov(np.array(x.values), self.dim)
+        if not np.isfinite(logdet_jit(self.cov_mat)):
+            return -np.inf
+        logP = self.prior.logpdf(self.cov_mat) + self.mu_prior
+        return logP
+    
+    def log_likelihood(self, x):
+        return log_integrand(self.mean[0], self.cov_mat, self.means, self.covs)
+
+def build_mean_cov(x, dim):
+    mean  = np.atleast_2d(x[:dim])
+    corr  = np.identity(dim)/2.
+    corr[np.triu_indices(dim, 1)] = x[2*dim:]
+    corr  = corr + corr.T
+    sigma = np.identity(dim)*x[dim:2*dim]**2
+    cov_mat = sigma@corr
+    return mean, cov_mat
+
+@njit
+def inv_jit(M):
+  return np.linalg.inv(M)
+
+@njit
+def logdet_jit(M):
+    return np.log(np.linalg.det(M))
+
+@njit
+def triple_product(v, M, n):
+    res = np.zeros(1, dtype = np.float64)
+    for i in prange(n):
+        for j in prange(n):
+            res = res + M[i,j]*v[i]*v[j]
+    return res
+
+@jit
+def log_norm(x, mu, cov):
+    inv_cov  = inv_jit(cov)
+    exponent = -0.5*triple_product(x, mu, inv_cov)
+    lognorm  = LOGSQRT2-0.5*logdet_jit(inv_cov)
+    return -lognorm+exponent
+
+@jit
+def log_integrand(mu, cov, means, sigmas):
+    logP = -np.inf
+    for i in prange(len(means)):
+        logP = log_add(logP, log_norm(means[i], mu, sigmas[i] + cov)))
+    return logP
