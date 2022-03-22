828ac02b5a8d0447fbdeffa655ba5a3fc7b8766e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-03-22 12:56:15 +0100

    modified handling

--- figaro/exceptions.py
+++ figaro/exceptions.py
@@ -3,11 +3,14 @@ import sys
 
 def except_hook(exctype, value, traceback):
     tb_last   = list(tb.walk_tb(traceback))[-1][0] # Get last call from traceback (function that raised the exception)
-    tb_s2last = list(tb.walk_tb(traceback))[-2][0] # Get second-to-last call from traceback (hopefully FIGARO function, to be checked)
+    try:
+        tb_s2last = list(tb.walk_tb(traceback))[-2][0] # Get second-to-last call from traceback (hopefully FIGARO function, to be checked)
+    except:
+        tb_s2last = ""
     # Check if error is due to some known improper usage of code
     #-----------#
     # Sample outside boundaries
-    if exctype == ValueError and tb_s2last.f_code.co_name == "assign_to_cluster" and tb_last.f_code.co_name == "numpy.random.mtrand.RandomState.choice":
+    if exctype == ValueError and tb_last.f_code.co_name == "numpy.random.mtrand.RandomState.choice" and tb_s2last.f_code.co_name == "assign_to_cluster":
         sys.__excepthook__(exctype, value, traceback)
         print("\nFIGARO: you probably have a sample that falls outside the given boundaries\n")
     else:
