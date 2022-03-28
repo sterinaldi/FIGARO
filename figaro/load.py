1f2969c98e12078b287a3fa37b161d1fe50350cb
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-03-28 17:32:02 +0200

    fix .dat loading

--- figaro/load.py
+++ figaro/load.py
@@ -10,7 +10,7 @@ def find_redshift(omega, dl):
         return dl - omega.LuminosityDistance_double(z)
     return newton(objective,1.0,args=(omega,dl))
 
-def load_single_event(event, seed = 0, par = 'm1', n_samples = -1, h = 0.674, om = 0.315, ol = 0.685):
+def load_single_event(event, seed = 0, par = ['m1'], n_samples = -1, h = 0.674, om = 0.315, ol = 0.685):
     '''
     Loads the data from .txt files (for simulations) or .h5/.hdf5 files (posteriors from GWTC) for a single event.
     Default cosmological parameters from Planck Collaboration (2021) in a flat Universe (https://www.aanda.org/articles/aa/pdf/2020/09/aa33910-18.pdf)
@@ -37,14 +37,14 @@ def load_single_event(event, seed = 0, par = 'm1', n_samples = -1, h = 0.674, om
         if n_samples > -1:
             samples = np.genfromtxt(event)
             s = int(min([n_samples, len(samples)]))
-            out = np.sort(rdstate.choice(samples, size = s, replace = False))
+            out = rdstate.choice(samples, size = s, replace = False)
         else:
-            out = np.sort(np.genfromtxt(event))
+            out = np.genfromtxt(event)
     else:
-        out = np.sort(unpack_gw_posterior(event, par = par, n_samples = n_samples, cosmology = (h, om, ol), rdstate = rdstate, ext = ext))
+        out = unpack_gw_posterior(event, par = par, n_samples = n_samples, cosmology = (h, om, ol), rdstate = rdstate, ext = ext)
     return out, name
 
-def load_data(path, seed = 0, par = 'm1', n_samples = -1, h = 0.674, om = 0.315, ol = 0.685):
+def load_data(path, seed = 0, par = ['m1'], n_samples = -1, h = 0.674, om = 0.315, ol = 0.685):
     '''
     Loads the data from .txt files (for simulations) or .h5/.hdf5 files (posteriors from GWTC).
     Default cosmological parameters from Planck Collaboration (2021) in a flat Universe (https://www.aanda.org/articles/aa/pdf/2020/09/aa33910-18.pdf)
@@ -72,9 +72,6 @@ def load_data(path, seed = 0, par = 'm1', n_samples = -1, h = 0.674, om = 0.315,
     names       = []
     n_events    = len(event_files)
     
-    empty_file_counter = 0
-    empty_files        = []
-    
     for i, event in enumerate(event_files):
         print('\r{0}/{1} event(s)'.format(i+1, n_events), end = '')
         name, ext = str(event).split('/')[-1].split('.')
@@ -83,28 +80,16 @@ def load_data(path, seed = 0, par = 'm1', n_samples = -1, h = 0.674, om = 0.315,
         
         if ext == 'txt':
             if n_samples > -1:
-                if not os.stat(event).st_size == 0:
-                    samples = np.atleast_1d(np.genfromtxt(event))
-                    s = int(min([n_samples, len(samples)]))
-                    events.append(np.sort(rdstate.choice(samples, size = s, replace = False)))
-                else:
-                    empty_file_counter += 1
-                    empty_files.append(str(event))
+                samples = np.atleast_1d(np.genfromtxt(event))
+                s = int(min([n_samples, len(samples)]))
+                events.append(rdstate.choice(samples, size = s, replace = False))
                     
             else:
-                if not os.stat(event).st_size == 0:
-                    samples = np.atleast_1d(np.genfromtxt(event))
-                    events.append(np.sort(samples))
-                else:
-                    empty_file_counter += 1
-                    empty_files.append(str(event))
+                samples = np.atleast_1d(np.genfromtxt(event))
+                events.append(samples)
                 
         else:
-            events.append(np.sort(unpack_gw_posterior(event, par = par, n_samples = n_samples, cosmology = (h, om, ol), rdstate = rdstate, ext = ext)))
-        
-    if empty_file_counter > 0:
-        print('\nWarning: {0} empty files detected: these events will not be processed.\nSee empty_files.txt for details.'.format(empty_file_counter))
-        np.savetxt(Path(path, 'empty_files.txt'), empty_files, fmt="%s")
+            events.append(unpack_gw_posterior(event, par = par, n_samples = n_samples, cosmology = (h, om, ol), rdstate = rdstate, ext = ext))
 
     return (events, np.array(names))
 
@@ -126,18 +111,28 @@ def unpack_gw_posterior(event, par, cosmology, rdstate, ext, n_samples = -1):
     omega = CosmologicalParameters(h, om, ol, -1, 0)
     if ext == 'h5' or ext == 'hdf5':
         with h5py.File(Path(event), 'r') as f:
+            samples = []
             try:
                 data = f['PublicationSamples']['posterior_samples']
-                if par == 'm1':
-                    samples = data['mass_1_source']
-                if par == 'm2':
-                    samples = data['mass_2_source']
-                if par == 'mc':
-                    samples = data['chirp_mass']
-                if par == 'z':
-                    samples = data['redshift']
-                if par == 'chi_eff':
-                    samples = data['chi_eff']
+                if 'm1' in par:
+                    samples.append(data['mass_1_source'])
+                if 'm2' in par:
+                    samples.append(data['mass_2_source'])
+                if 'mc' in par:
+                    samples.append(data['chirp_mass'])
+                if 'z' in par:
+                    samples.append(data['redshift'])
+                if 'chi_eff' in par:
+                    samples.append(data['chi_eff'])
+                if 'ra' in par:
+                    samples.append(data['ra'])
+                if 'dec' in par:
+                    samples.append(data['dec'])
+                if 'luminosity_distance' in par:
+                    samples.append(data['luminosity_distance'])
+                
+                samples = np.array(samples).T
+
                 if n_samples > -1:
                     s = int(min([n_samples, len(samples)]))
                     return rdstate.choice(samples, size = s, replace = False)
@@ -145,6 +140,8 @@ def unpack_gw_posterior(event, par, cosmology, rdstate, ext, n_samples = -1):
                     return samples
             except:
                 data = f['Overall_posterior']
+                ra        = data['right_ascension']
+                dec       = data['declination']
                 LD        = data['luminosity_distance_Mpc']
                 z         = np.array([find_redshift(omega, l) for l in LD])
                 m1_detect = data['m1_detector_frame_Msun']
@@ -167,7 +164,15 @@ def unpack_gw_posterior(event, par, cosmology, rdstate, ext, n_samples = -1):
                     cos2 = data['costilt2']
                     q    = m2/m1
                     samples = (s1*cos1 + q*s2*cos2)/(1+q)
+                if 'ra' in par:
+                    samples.append(ra)
+                if 'dec' in par:
+                    samples.append(dec)
+                if 'luminosity_distance' in par:
+                    samples.append(LD)
                 
+                samples = np.array(samples).T
+
                 if n_samples > -1:
                     s = int(min([n_samples, len(samples)]))
                     return rdstate.choice(samples, size = s, replace = False)
@@ -175,10 +180,34 @@ def unpack_gw_posterior(event, par, cosmology, rdstate, ext, n_samples = -1):
                     return samples
     else:
         data = np.genfromtxt(Path(event), names = True)
-        if par == 'm1':
-            samples = data['mass_1']
-        if par == 'm2':
-            samples = data['mass_2']
+        
+        samples = []
+        
+        ra        = data['ra']
+        dec       = data['dec']
+        LD        = data['luminosity_distance']
+        z         = np.array([find_redshift(omega, l) for l in LD])
+        m1_detect = data['mass_1']
+        m2_detect = data['mass_2']
+        m1        = m1_detect/(1+z)
+        m2        = m2_detect/(1+z)
+        
+        if 'z' in par:
+            samples.append(z)
+        if 'm1' in par:
+            samples.append(m1)
+        if 'm2' in par:
+            samples.append(m2)
+        if 'mc' in par:
+            samples.append((m1*m2)**(3./5.)/(m1+m2)**(1./5.))
+        if 'ra' in par:
+            samples.append(ra)
+        if 'dec' in par:
+            samples.append(dec)
+        if 'luminosity_distance' in par:
+            samples.append(LD)
+        
+        samples = np.array(samples).T
         
         if n_samples > -1:
             s = int(min([n_samples, len(samples)]))
