660d0c652113596008a9a285ff3f7e250a9e1be9
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-04-20 13:08:06 +0200

    added option saving methods and multidim cornerplot

--- figaro/utils.py
+++ figaro/utils.py
@@ -4,6 +4,8 @@ import warnings
 from pathlib import Path
 from distutils.spawn import find_executable
 from matplotlib import rcParams
+from corner import corner
+from collections import Counter
 
 if find_executable('latex'):
     rcParams["text.usetex"] = True
@@ -16,7 +18,48 @@ rcParams["axes.labelsize"]=16
 rcParams["axes.grid"] = True
 rcParams["grid.alpha"] = 0.6
 
-def plot_median_cr(draws, injected = None, samples = None, bounds = None, out_folder = '.', name = 'density', n_pts = 1000, label = 'x', unit = None, hierarchical = False, show = False, save = True):
+#-------------#
+#   Options   #
+#-------------#
+
+def is_opt_provided(parser, dest):
+    """
+    Checks if an option is provided by the user.
+    From Oleg Gryb's answer in
+    https://stackoverflow.com/questions/2593257/how-to-know-if-optparse-option-was-passed-in-the-command-line-or-as-a-default
+    
+    Arguments:
+        :obj parser: an instance of optparse.OptionParser with the user-provided options
+        :str dest:   name of the option
+    Returns:
+        :bool: True if the option is provided, false otherwise
+    """
+    for opt in parser._get_all_options():
+        try:
+            if opt.dest == dest and (opt._long_opts[0] in sys.argv[1:] or opt._short_opts[0] in sys.argv[1:]):
+                return True
+        except:
+            if opt.dest == dest and opt._long_opts[0] in sys.argv[1:]:
+                return True
+    return False
+
+def save_options(options):
+    """
+    Saves options for the run (reproducibility)
+    
+    Arguments:
+        :dict options: options
+    """
+    logfile = open(Path(options.output, 'options_log.txt'), 'w')
+    for key, val in zip(vars(options).keys(), vars(options).values()):
+        logfile.write('{0}: {1}\n'.format(key,val))
+    logfile.close()
+
+#-------------#
+#    Plots    #
+#-------------#
+
+def plot_median_cr(draws, injected = None, samples = None, bounds = None, out_folder = '.', name = 'density', n_pts = 1000, label = None, unit = None, hierarchical = False, show = False, save = True):
     
     if hierarchical:
         rec_label = '\mathrm{(H)DPGMM}'
@@ -72,6 +115,8 @@ def plot_median_cr(draws, injected = None, samples = None, bounds = None, out_fo
     # Median
     ax.plot(x, p[50], lw = 0.7, color = 'steelblue', label = '${0}$'.format(rec_label))
     
+    if label is None:
+        label = 'x'
     if unit is None:
         ax.set_xlabel('${0}$'.format(label))
     else:
@@ -87,3 +132,44 @@ def plot_median_cr(draws, injected = None, samples = None, bounds = None, out_fo
         fig.savefig(Path(out_folder, 'log_{0}.pdf'.format(name)), bbox_inches = 'tight')
         plt.close()
         np.savetxt(Path(out_folder, 'prob_{0}.txt'.format(name)), np.array([x, p[50], p[5], p[16], p[84], p[95]]).T, header = 'x 50 5 16 84 95')
+
+def plot_multidim(draws, dim, samples = None, out_folder = '.', name = 'density', labels = None, units = None, hierarchical = False, show = False, save = True):
+
+    if hierarchical:
+        rec_label = '\mathrm{(H)DPGMM}'
+    else:
+        rec_label = '\mathrm{DPGMM}'
+    
+    if labels is None:
+        labels = ['$x_{0}$'.format(i+1) for i in range(dim)]
+    else:
+        labels = ['${0}$'.format(l) for l in labels]
+    
+    if units is not None:
+        labels = [l[:-1]+'\ [{0}]$'.format(u) for l, u in zip(labels, units)]
+    
+    # Draw samples from mixture
+    if samples is not None:
+        size = np.min([1000, len(samples)])
+    else:
+        size = 1000
+        
+    idx = np.random.choice(np.arange(len(draws)), size = size)
+    ctr = Counter(idx)
+    
+    mix_samples = np.empty(shape = (1,dim))
+    for i, n in zip(ctr.keys(), ctr.values()):
+        mix_samples = np.concatenate((mix_samples, draws[i].sample_from_dpgmm(n)))
+    mix_samples = mix_samples[1:]
+    
+    # Make corner plots
+    if samples is not None:
+        c = corner(samples, color = 'coral', labels = labels, hist_kwargs={'density':True, 'label':'$\mathrm{Samples}$'})
+        c = corner(mix_samples, fig = c, color = 'dodgerblue', labels = labels, hist_kwargs={'density':True, 'label':'${0}$'.format(rec_label)})
+    else:
+        c = corner(mix_samples, color = 'dodgerblue', labels = labels, hist_kwargs={'density':True, 'label':'${0}$'.format(rec_label)})
+    plt.legend(loc = 0, frameon = False, fontsize = 12, bbox_to_anchor = (0.95, (dim-1)+0.8))
+    if show:
+        plt.show()
+    if save:
+        c.savefig(Path(out_folder, '{0}.pdf'.format(name)), bbox_inches = 'tight')
