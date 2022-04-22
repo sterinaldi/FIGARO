b8c1cf629399d971b8c08a73c64838c83e149eaa
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-04-22 15:28:34 +0200

    added **kwargs

--- figaro/decorators.py
+++ figaro/decorators.py
@@ -10,7 +10,7 @@ def antiprobit(func):
     """
     Transform a point x from probit space to natural space and returns the function evaluated at the natural point y
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = transform_from_probit(x, ref.bounds)
         return func(ref, y, *args)
     return f_transf
@@ -19,7 +19,7 @@ def probit(func):
     """
     Transform a point x from natural space to probit space and returns the function evaluated at the probit point y
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = transform_to_probit(x, ref.bounds)
         return func(ref, y, *args)
     return f_transf
@@ -28,7 +28,7 @@ def from_probit(func):
     """
     Evaluate a function that samples points in probit space and return these points after transforming them to natural space
     """
-    def f_transf(ref, *args):
+    def f_transf(ref, *args, **kwargs):
         y = func(ref, *args)
         return transform_from_probit(y, ref.bounds)
     return f_transf
@@ -37,7 +37,7 @@ def jacobian_probit(func):
     """
     Evaluates a function and returns it multiplied by the jacobian of the probit transformation
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = func(ref, x)
         return y*np.exp(probit_logJ(x, ref.bounds))
     return f_transf
@@ -46,7 +46,7 @@ def jacobian_log_probit(func):
     """
     Evaluates a function and returns the logarithm of it multiplied by the jacobian of the probit transformation
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = func(ref, x)
         return y + probit_logJ(x, ref.bounds)
     return f_transf
@@ -55,7 +55,7 @@ def inv_jacobian_probit(func):
     """
     Evaluates a function and returns it multiplied by the inverse of the jacobian of the probit transformation
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = func(ref, x)
         return y*np.exp(-probit_logJ(x, ref.bounds))
     return f_transf
@@ -64,7 +64,7 @@ def inv_jacobian_log_probit(func):
     """
     Evaluates a function and returns the logarithm of it multiplied by the jacobian of the probit transformation
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = func(ref, x)
         return y - probit_logJ(x, ref.bounds)
     return f_transf
@@ -73,7 +73,7 @@ def cartesian(func):
     """
     Transform a point x from celestial coordinates to cartesian coordinates and returns the function evaluated at the probit point y
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = celestial_to_cartesian(x)
         return func(ref, y, *args)
     return f_transf
@@ -82,7 +82,7 @@ def celestial(func):
     """
     Transform a point x from cartesian coordinates to celestial coordinates and returns the function evaluated at the probit point y
     """
-    def f_transf(ref, x, *args):
+    def f_transf(ref, x, *args, **kwargs):
         y = cartesian_to_celestial(x)
         return func(y, *args)
     return f_transf
