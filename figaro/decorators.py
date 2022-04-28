98208813e2a8e5520c344069e4aecdf0aeb50a8e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-04-28 18:23:08 +0200

    removed unused decorators

--- figaro/decorators.py
+++ figaro/decorators.py
@@ -1,6 +1,5 @@
-from figaro.transform import transform_from_probit, transform_to_probit, probit_logJ
+from figaro.transform import transform_from_probit, transform_to_probit
 from figaro.coordinates import celestial_to_cartesian, cartesian_to_celestial
-import numpy as np
 
 """
 A gentle introduction to decorators: https://towardsdatascience.com/how-to-use-decorators-in-python-by-example-b398328163b
@@ -31,42 +30,6 @@ def from_probit(func):
     def f_transf(ref, *args, **kwargs):
         y = func(ref, *args)
         return transform_from_probit(y, ref.bounds)
-    return f_transf
-
-def jacobian_probit(func):
-    """
-    Evaluates a function and returns it multiplied by the jacobian of the probit transformation
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = func(ref, x)
-        return y*np.exp(probit_logJ(x, ref.bounds))
-    return f_transf
-
-def jacobian_log_probit(func):
-    """
-    Evaluates a function and returns the logarithm of it multiplied by the jacobian of the probit transformation
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = func(ref, x)
-        return y + probit_logJ(x, ref.bounds)
-    return f_transf
-
-def inv_jacobian_probit(func):
-    """
-    Evaluates a function and returns it multiplied by the inverse of the jacobian of the probit transformation
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = func(ref, x)
-        return y*np.exp(-probit_logJ(x, ref.bounds))
-    return f_transf
-
-def inv_jacobian_log_probit(func):
-    """
-    Evaluates a function and returns the logarithm of it multiplied by the jacobian of the probit transformation
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = func(ref, x)
-        return y - probit_logJ(x, ref.bounds)
     return f_transf
 
 def cartesian(func):
