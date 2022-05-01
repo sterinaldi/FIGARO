8d54ca6c3dec99a06fca803823806eec9feade7e
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-05-01 19:44:47 +0200

    added measures

--- figaro/credible_regions.py
+++ figaro/credible_regions.py
@@ -81,7 +81,7 @@ def FindLevelForHeight(inLogArr, logvalue, logdd):
     idx = (np.abs(adSorted-logvalue)).argmin()
     return np.exp(adCum[idx])
 
-def ConfidenceVolume(log_volume_map, ra_grid, dec_grid, distance_grid, adLevels = [0.50, 0.90]):
+def ConfidenceVolume(log_volume_map, log_measure, ra_grid, dec_grid, distance_grid, adLevels = [0.50, 0.90]):
     """
     Compute the credible volume(s) for a 3D probability distribution
     
@@ -102,26 +102,23 @@ def ConfidenceVolume(log_volume_map, ra_grid, dec_grid, distance_grid, adLevels 
     dra = np.diff(ra_grid)[0]
     # create a normalized cumulative distribution
     log_volume_map_sorted = np.ascontiguousarray(np.sort(log_volume_map.flatten())[::-1])
-    log_volume_map_cum = fast_log_cumulative(log_volume_map_sorted + np.log(dra) + np.log(ddec) + np.log(dd))
-    
+    log_measure_sorted    = np.ascontiguousarray(log_measure.flatten()[np.argsort(log_volume_map.flatten())][::-1])
+    log_volume_map_cum    = fast_log_cumulative(log_volume_map_sorted + log_measure_sorted + np.log(dra) + np.log(ddec) + np.log(dd))
     # find the indeces  corresponding to the given CLs
     adLevels = np.ravel([adLevels])
     args = [(log_volume_map_sorted, log_volume_map_cum, level) for level in adLevels]
     adHeights = [FindHeights(a) for a in args]
-    heights = {str(lev):hei for lev,hei in zip(adLevels,adHeights)}
     volumes         = []
     index           = []
     for height in adHeights:
-        
         (i_ra, i_dec, i_d,) = np.where(log_volume_map>=height)
         volumes.append(np.sum([distance_grid[i_d]**2. *np.cos(dec_grid[i_dec]) * dd * dra * ddec for i_d,i_dec in zip(i_d,i_dec)]))
         index.append(np.array([i_ra, i_dec, i_d]).T)
-
     volume_confidence = np.array(volumes)
     
     return volume_confidence, index, np.array(adHeights)
 
-def ConfidenceArea(log_skymap, ra_grid, dec_grid, adLevels = [0.50, 0.90]):
+def ConfidenceArea(log_skymap, log_measure, ra_grid, dec_grid, adLevels = [0.50, 0.90]):
     """
     Compute the credible area(s) for a 2D probability distribution
     
@@ -140,24 +137,23 @@ def ConfidenceArea(log_skymap, ra_grid, dec_grid, adLevels = [0.50, 0.90]):
     ddec = np.diff(dec_grid)[0]
     dra = np.diff(ra_grid)[0]
     log_skymap_sorted = np.ascontiguousarray(np.sort(log_skymap.flatten())[::-1])
-    log_skymap_cum = fast_log_cumulative(log_skymap_sorted + np.log(dra) + np.log(ddec))
+    log_measure_sorted = np.ascontiguousarray(log_measure.flatten()[np.argsort(log_skymap.flatten())][::-1])
+    log_skymap_cum = fast_log_cumulative(log_skymap_sorted + log_measure_sorted.flatten() + np.log(dra) + np.log(ddec))
     # find the indeces  corresponding to the given CLs
     adLevels = np.ravel([adLevels])
     args = [(log_skymap_sorted, log_skymap_cum, level) for level in adLevels]
     adHeights = [FindHeights(a) for a in args]
     areas = []
     index = []
-                
     for height in adHeights:
         (i_ra,i_dec,) = np.where(log_skymap>=height)
         areas.append(np.sum([dra*np.cos(dec_grid[i_d])*ddec for i_d in i_dec])*(180.0/np.pi)**2.0)
-
         index.append(np.array([i_ra, i_dec]).T)
     area_confidence = np.array(areas)
     
     return area_confidence, index, np.array(adHeights)
 
-def ConfidenceInterval(probability, grid, adLevels = [0.50, 0.90]):
+def ConfidenceInterval(probability, measure, grid, adLevels = [0.50, 0.90]):
     """
     Compute the credible interval(s) for a 1D probability distribution
     
@@ -171,7 +167,7 @@ def ConfidenceInterval(probability, grid, adLevels = [0.50, 0.90]):
         :iterable:   indices of bins within credible area(s)
     """
     dx = np.diff(grid)[0]
-    cumulative_distribution = np.cumsum(probability*dx)
+    cumulative_distribution = np.cumsum(probability*dx*measure)
     values = []
     index  = []
     for cl in adLevels:
