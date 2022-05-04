07ef16c7ca5ea8f1b61ba578d20b00c9c495a512
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-05-04 09:31:34 +0200

    jit

--- figaro/montecarlo.py
+++ figaro/montecarlo.py
@@ -1,49 +1,11 @@
 import numpy as np
 from numba import jit, njit, prange
-from numba.extending import get_cython_function_address
-import ctypes
-from scipy.stats import invgamma, invwishart, multivariate_normal
-from scipy.special import logsumexp
 
 LOG2PI = np.log(2*np.pi)
 
 #-----------#
 # Functions #
 #-----------#
-
-@jit
-def _log_add(x, y):
-    """
-    Compute log(np.exp(x) + np.exp(y))
-    
-    Arguments:
-        :double x: first addend (log)
-        :double y: second addend (log)
-    
-    Returns:
-        :double: log(np.exp(x) + np.exp(y))
-    """
-    if x >= y:
-        return x+np.log1p(np.exp(y-x))
-    else:
-        return y+np.log1p(np.exp(x-y))
-
-@jit
-def _log_add_array(x,y):
-    """
-    Compute log(np.exp(x) + np.exp(y)) element-wise
-    
-    Arguments:
-        :np.ndarray x: first addend (log)
-        :np.ndarray y: second addend (log)
-    
-    Returns:
-        :np.ndarray: log(np.exp(x) + np.exp(y)) element-wise
-    """
-    res = np.zeros(len(x), dtype = np.float64)
-    for i in prange(len(x)):
-        res[i] = _log_add(x[i],y[i])
-    return res
 
 @jit
 def log_norm_1d(x, m, s):
@@ -106,46 +68,37 @@ def log_norm(x, mu, cov):
     return -lognorm+exponent
 
 @jit
-def log_norm_array(x, mu, cov):
-    """
-    Multivariate Normal logpdf element-wise wrt mu and cov
-    
-    Arguments:
-        :np.ndarray x: value
-        :np.ndarray m: vector of mean vectors
-        :np.ndarray s: vector of covariance matrices
-    
-    Returns:
-        :np.ndarray: MultivariateNormal(m,s).logpdf(x)
-    """
-    vect = np.zeros(len(mu), dtype = np.float64)
-    for i in prange(len(mu)):
-        vect[i] = log_norm(x, mu[i], cov[i])
-    return vect
+def logsumexp_jit(a, b):
+    a_max = np.max(a)
+    tmp = b * np.exp(a - a_max)
+    return np.log(np.sum(tmp)) + a_max
 
 #------------#
 # 1D methods #
 #------------#
 
-
-def compute_probability_1d(mu, sigma, means, vars, log_w):
+@jit
+def evaluate_mixture_MC_draws_1d(mu, sigma, means, vars, w):
     logP = np.zeros(len(mu), dtype = np.float64)
-    for i, (m, s) in enumerate(zip(mu, sigma)):
-        logP[i] = eval_mix_1d(m, s, means, vars, log_w)
+    for i in prange(len(mu)):
+        logP[i] = logsumexp_jit(eval_mix_1d(mu[i], sigma[i], means, vars), b = w)
     return logP
 
-def eval_mix_1d(mu, sigma, means, vars, log_w):
-    return logsumexp([log_norm_1d(means[i,0], mu, sigma+vars[i,0,0]) for i in range(len(means))], b = np.exp(log_w))
+@jit
+def eval_mix_1d(mu, sigma, means, vars):
+    return np.array([log_norm_1d(means[i,0], mu, sigma+vars[i,0,0]) for i in prange(len(means))])
 
 #------------#
 # ND methods #
 #------------#
 
-def compute_probability(mu, sigma, means, vars, log_w):
+@jit
+def evaluate_mixture_MC_draws(mu, sigma, means, vars, w):
     logP = np.zeros(len(mu), dtype = np.float64)
-    for i, (m, s) in enumerate(zip(mu, sigma)):
-        logP[i] = eval_mix(m, s, means, vars, log_w)
+    for i in prange(len(mu)):
+        logP[i] = logsumexp_jit(eval_mix(mu[i], sigma[i], means, vars), b = w)
     return logP
 
-def eval_mix(mu, sigma, means, vars, log_w):
-    return logsumexp([log_norm(means[i], mu, sigma+vars[i]) for i in range(len(means))], b = log_w)
+@jit
+def eval_mix(mu, sigma, means, vars):
+    return np.array([log_norm(means[i], mu, sigma+vars[i]) for i in prange(len(means))], b = log_w)
