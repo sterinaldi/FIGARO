594c2f94e965693d8c1ead876ff121763dd7c97f
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-05-06 23:34:41 +0200

    fixed bug in initialise multidim

--- figaro/mixture.py
+++ figaro/mixture.py
@@ -704,13 +704,13 @@ class HDPGMM(DPGMM):
                        prior_pars = None,
                        MC_draws   = 1e3,
                        ):
-        dim = len(bounds)
+        self.dim = len(bounds)
         if prior_pars == None:
-            prior_pars = (1e-2, np.identity(dim)*0.2**2, dim+2, np.zeros(dim))
+            prior_pars = (1e-2, np.identity(self.dim)*0.2**2, dim+2, np.zeros(self.dim))
         super().__init__(bounds = bounds, prior_pars = prior_pars, alpha0 = alpha0, out_folder = out_folder)
         self.MC_draws = int(MC_draws)
         
-        self.sigma_MC = invwishart(df = self.prior.nu-dim-1, scale = self.prior.L).rvs(size = self.MC_draws)
+        self.sigma_MC = invwishart(df = self.prior.nu-self.dim-1, scale = self.prior.L).rvs(size = self.MC_draws)
         if self.dim == 1:
             self.mu_MC    = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
         else:
@@ -724,7 +724,7 @@ class HDPGMM(DPGMM):
             self.sigma_MC = invgamma(self.prior.nu/2, scale = self.prior.nu*self.prior.L[0,0]/2.).rvs(size = self.MC_draws)
             self.mu_MC    = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
         else:
-            df = np.max([self.prior.nu, dim + 2])
+            df = np.max([self.prior.nu, self.dim + 2])
             self.sigma_MC = invwishart(df = df, scale = self.prior.L*(df-dim-1)).rvs(size = self.MC_draws)
             self.mu_MC    = np.array([mn(self.prior.mu, s/self.prior.k).rvs() for s in self.sigma_MC])
     
