5d489e2722d057fddbcba43ac1da939574c64cec
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-05-22 12:42:47 +0200

    added comments

--- figaro/marginal.py
+++ figaro/marginal.py
@@ -3,6 +3,16 @@ from figaro.mixture import mixture
 from figaro.exceptions import FIGAROException
 
 def _marginalise(mix, axis = -1):
+    """
+    Marginalises out one or more dimensions from a FIGARO draw.
+    
+    Arguments:
+        :figaro.mixture.mixture draws: mixture
+        :int or list of int axis:      axis to marginalise on
+    
+    Returns:
+        :figaro.mixture.mixture: the marginalised mixture
+    """
     ax     = np.atleast_1d(axis)
     means  = np.delete(mix.means, ax, axis = -1)
     covs   = np.delete(np.delete(mix.covs, ax, axis = -1), ax, axis = -2)
@@ -14,6 +24,16 @@ def _marginalise(mix, axis = -1):
     return mixture(means, covs, mix.w, bounds, dim, mix.n_cl, mix.n_pts)
 
 def marginalise(draws, axis = -1):
+    """
+    Marginalises out one or more dimensions from a FIGARO draw.
+    
+    Arguments:
+        :figaro.mixture.mixture draws: mixture(s)
+        :int or list of int axis:      axis to marginalise on
+    
+    Returns:
+        :figaro.mixture.mixture: the marginalised mixture(s)
+    """
     if np.iterable(draws):
         return np.array([_marginalise(d, axis) for d in draws])
     else:
