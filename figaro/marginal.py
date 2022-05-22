1f9f69157c3a3f635ae0c713df9219c06f6beaa6
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-05-22 12:48:13 +0200

    added comments

--- figaro/marginal.py
+++ figaro/marginal.py
@@ -14,11 +14,11 @@ def _marginalise(mix, axis = -1):
         :figaro.mixture.mixture: the marginalised mixture
     """
     ax     = np.atleast_1d(axis)
-    means  = np.delete(mix.means, ax, axis = -1)
-    covs   = np.delete(np.delete(mix.covs, ax, axis = -1), ax, axis = -2)
     dim    = mix.dim - len(ax)
     if dim < 1:
         raise FIGAROException("Cannot marginalise out all dimensions")
+    means  = np.delete(mix.means, ax, axis = -1)
+    covs   = np.delete(np.delete(mix.covs, ax, axis = -1), ax, axis = -2)
     bounds = np.delete(mix.bounds, ax, axis = 0)
     
     return mixture(means, covs, mix.w, bounds, dim, mix.n_cl, mix.n_pts)
