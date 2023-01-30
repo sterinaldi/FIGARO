52a58f264a40c28bf1c17e000688af377a956ac3
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-01-30 16:57:37 +0100

    added support for h5 true values file

--- figaro/pipelines/par_hierarchical_inference.py
+++ figaro/pipelines/par_hierarchical_inference.py
@@ -183,7 +183,7 @@ def main():
         true_vals, true_name = load_data(options.true_vals, par = options.par, h = options.h, om = options.om, ol = options.ol, waveform = options.wf)
         if np.shape(true_vals)[-1] == 1:
             true_vals = true_vals.flatten()
-    
+            print(true_vals)
     # Load samples
     events, names = load_data(options.samples_folder, par = options.par, n_samples = options.n_samples_dsp, h = options.h, om = options.om, ol = options.ol, waveform = options.wf, snr_threshold = options.snr_threshold, far_threshold = options.far_threshold)
     try:
