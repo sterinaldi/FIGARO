d075aab90d6dc0be5fbf7c22dc19b0c9b2ef9978
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-02-08 13:48:34 +0100

    updated

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@ if [ "$1" = "-e" ]; then
   env_flag=true
 fi
 
+# If environment:
 if $env_flag; then
     if ! ( conda env list | grep ".*figaro_env.*" >/dev/null 2>&1); then
         conda env create -f figaro_env.yml
     fi
     conda activate figaro_env
+else
+    pip install -r requirements
 fi
+
 conda install -c conda-forge -y -S lalsuite
 {python setup.py install} || {python setup.py install --user}
 
