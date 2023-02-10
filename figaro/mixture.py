ec6f314ab9bf0d9a38bdc50966e00434c9593d82
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-02-10 16:57:37 +0100

    properly rescaled covariance matrix with k

--- figaro/mixture.py
+++ figaro/mixture.py
@@ -78,6 +78,15 @@ def _student_t(df, t, mu, sigma, dim):
     return (A - B - C - D + E)[0]
 
 @jit
+def rescale_covariance(covs, k):
+    out_covs = np.zeros(shape = np.shape(covs), dtype = np.float64)
+    for i, c in enumerate(covs):
+        diag = np.diag(c)
+        rho  = c/np.outer(diag, diag)
+        out_covs[i] = rho*np.outer(diag/np.sqrt(k), diag/np.sqrt(k))
+    return out_covs
+
+@jit
 def update_alpha(alpha, n, K, burnin = 1000):
     """
     Update concentration parameter using a Metropolis-Hastings sampling scheme.
@@ -843,9 +852,9 @@ class HDPGMM(DPGMM):
         df = np.max([self.prior.nu, self.dim + 2])
         self.sigma_MC = invwishart(df = df, scale = self.prior.L).rvs(size = self.MC_draws)
         if self.dim == 1:
-            self.mu_MC    = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
+            self.mu_MC = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
         else:
-            self.mu_MC    = np.array([mn(self.prior.mu, s/self.prior.k).rvs() for s in self.sigma_MC])
+            self.mu_MC = np.array([mn(self.prior.mu, s).rvs() for s in rescale_covariance(self.sigma_MC, self.prior.k)])
         # For logsumexp_jit
         self.b_ones = np.ones(self.MC_draws)
     
@@ -854,9 +863,9 @@ class HDPGMM(DPGMM):
         df = np.max([self.prior.nu, self.dim + 2])
         self.sigma_MC = invwishart(df = df, scale = self.prior.L).rvs(size = self.MC_draws)
         if self.dim == 1:
-            self.mu_MC    = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
+            self.mu_MC = np.array([np.random.normal(loc = self.prior.mu[0], scale = s) for s in np.sqrt(self.sigma_MC/self.prior.k)])
         else:
-            self.mu_MC    = np.array([mn(self.prior.mu, s/self.prior.k).rvs() for s in self.sigma_MC])
+            self.mu_MC = np.array([mn(self.prior.mu, s).rvs() for s in rescale_covariance(self.sigma_MC, self.prior.k)])
     
     def add_new_point(self, ev):
         """
