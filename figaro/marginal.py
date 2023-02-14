13785a3499fba8e872695cc169abb6e6d7d1e4a6
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-02-14 15:31:28 +0100

    working conditional distribution

--- figaro/marginal.py
+++ figaro/marginal.py
@@ -1,7 +1,32 @@
 import numpy as np
+from numba import njit
 from figaro.mixture import mixture
 from figaro.exceptions import FIGAROException
+from figaro.likelihood import log_norm, inv_jit
 
+@njit
+def _cond_mean_cov(vals, mu1, mu2, s11, s22, s12):
+    """
+    Compute mean and covariance for conditional Normal distribution.
+    See https://stats.stackexchange.com/questions/348941/general-conditional-distributions-for-multivariate-gaussian-mixtures
+    
+    Arguments:
+        :np.ndarray vals: values to condition on
+        :np.ndarray mu1:  mean subvector (NOT conditioned)
+        :np.ndarray mu2:  mean subvector (conditioned)
+        :np.ndarray s11:  covariance submatrix (NOT conditioned)
+        :np.ndarray s22:  covariance submatrix (conditioned)
+        :np.ndarray s12:  off-diagonal
+    
+    Returns:
+        :np.ndarray: mean
+        :np.ndarray: covariance
+    """
+    s22_inv = inv_jit(s22)
+    mean = mu1 + s12@(s22_inv@(vals - mu2))
+    cov  = s11 - s12@(s22_inv@s12.T)
+    return mean, cov
+    
 def _marginalise(mix, axis = -1):
     """
     Marginalises out one or more dimensions from a FIGARO draw.
@@ -40,3 +65,65 @@ def marginalise(draws, axis = -1):
         return np.array([_marginalise(d, axis) for d in draws])
     else:
         return _marginalise(draws, axis)
+
+def condition(draws, vals, dims, norm = True):
+    """
+    Returns the probability density conditioned on specific values of a subset of parameters.
+    
+    Arguments:
+        :figaro.mixture.mixture draws: mixture(s)
+        :iterable vals:                value(s) to condition on
+        :int or list of int dims:      dimension(s) associated with given vals (starting from 0)
+        :bool norm:                    normalize the distribution
+    
+    Returns:
+        :figaro.mixture.mixture: the conditioned mixture(s)
+    """
+    if np.iterable(draws):
+        return np.array([_condition(d, vals, dims, norm) for d in draws])
+    else:
+        return _condition(draws, vals, dims, norm)
+
+def _condition(mix, vals, dims, norm = True):
+    """
+    Returns the probability density conditioned on specific values of a subset of parameters.
+    See:
+     * https://stats.stackexchange.com/questions/348941/general-conditional-distributions-for-multivariate-gaussian-mixtures
+     * https://stats.stackexchange.com/questions/30588/deriving-the-conditional-distributions-of-a-multivariate-normal-distribution
+    
+    Arguments:
+        :figaro.mixture.mixture mix: mixture
+        :iterable vals:              value(s) to condition on
+        :int or list of int dims:    dimension(s) associated with given vals (starting from 0)
+        :bool norm:                  normalize the distribution
+    
+    Returns:
+        :figaro.mixture.mixture: the conditioned mixture(s)
+    """
+    vals = np.atleast_1d(vals)
+    ax   = np.atleast_1d(dims)
+    dim  = mix.dim - len(ax)
+    idx  = np.array([i in ax for i in range(mix.dim)])
+    if dim < 1:
+        raise FIGAROException("Cannot condition on all dimensions")
+    bounds    = np.delete(mix.bounds, ax, axis = 0)
+    means     = np.zeros(shape = (mix.n_cl, dim))
+    covs      = np.zeros(shape = (mix.n_cl, dim, dim))
+    weights   = np.zeros(shape = mix.n_cl)
+    if norm:
+        marg_dist = _marginalise(mix, axis = np.arange(mix.dim)[~idx])
+    for i, (mu, cov) in enumerate(zip(mix.means, mix.covs)):
+        # Subvectors and submatrices
+        mu1 = mu[~idx]
+        mu2 = mu[idx]
+        s11 = cov[~idx,:][:,~idx]
+        s12 = cov[idx,:][:,~idx]
+        s22 = cov[idx,:][:,idx]
+        # Parameters
+        means[i], covs[i] = _cond_mean_cov(vals, mu1, mu2, s11, s22, s12)
+        weights[i] = np.exp(log_norm(vals, mu2, s22))
+    # Weights
+    weights = mix.w*weights
+    if norm:
+        weights /= marg_dist.pdf(vals)
+    return mixture(means, covs, weights, bounds, dim, mix.n_cl, mix.n_pts, probit = mix.probit)
