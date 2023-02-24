a75c7099d84c5236736623d3e6b30d58ddba7eb5
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-02-24 14:55:45 +0100

    load snr - fixed bug

--- figaro/load.py
+++ figaro/load.py
@@ -330,8 +330,10 @@ def _unpack_gw_posterior(event, par, cosmology, rdstate, n_samples = -1, wavefor
                         try:
                             if MDC_flag:
                                 snr = np.array(data['snr'])
+                                samples.append(data[lab])
                             elif waveform != 'combined':
                                 snr = np.array(data[lab])
+                                samples.append(data[lab])
                             if snr_threshold is not None:
                                 flag_filter = True
                         except:
