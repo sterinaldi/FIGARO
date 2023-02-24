144cbbd4c7ab0abfd25cb2a2ed646614638e071c
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2023-02-24 15:03:55 +0100

    load snr - fixed bug

--- figaro/load.py
+++ figaro/load.py
@@ -337,6 +337,7 @@ def _unpack_gw_posterior(event, par, cosmology, rdstate, n_samples = -1, wavefor
                                 print(data[lab])
                             if snr_threshold is not None:
                                 flag_filter = True
+                            print(samples)
                         except:
                             warnings.warn("SNR filter is not available with this dataset.")
                     if name == 'far' and far_threshold is not None:
@@ -433,8 +434,9 @@ def _unpack_gw_posterior(event, par, cosmology, rdstate, n_samples = -1, wavefor
 
             for name in GW_par.keys():
                 if name in par:
-                    samples.append(ss[name])
-                    loaded_pars.append(name)
+                    if not (name == 'snr' or name == 'far'):
+                        samples.append(ss[name])
+                        loaded_pars.append(name)
 
             if len(par) == 1:
                 samples = np.atleast_2d(samples).T
