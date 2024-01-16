a094e14577f0ea4294b4dd173aead06b5671658c
sterinaldi <stefano.rinaldi@unipd.it>
2024-01-16 17:52:42 +0100

    updated with tests

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ env_flag=false
 if [ "$1" = "-e" ]; then
   env_flag=true
 fi
-
 # If environment:
 if $env_flag; then
     if ! ( conda env list | grep ".*figaro_env.*" >/dev/null 2>&1); then
@@ -11,21 +10,18 @@ if $env_flag; then
     if [[ $CONDA_DEFAULT_ENV!='figaro_env' ]]; then
         conda activate figaro_env
     fi
-
+fi
 # Install lalsuite from CONDA
 conda install -c conda-forge -y -S lalsuite
-
 {
 pip install -r requirements.txt
 } || {
 pip install --user -r requirements.txt
 }
-
 python setup.py build_ext --inplace
-
 {
 python setup.py install
 } || {
 python setup.py install --user
 }
-
+python setup.py test
