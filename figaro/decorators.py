92b2ad1f272f3e44b0533cb4a8d9c40d4f2ae4a8
sterinaldi <stefano.rinaldi@unipd.it>
2024-01-17 12:35:23 +0100

    removed references to cartesian coordinate change

--- figaro/decorators.py
+++ figaro/decorators.py
@@ -1,5 +1,4 @@
 from figaro.transform import transform_from_probit, transform_to_probit
-from figaro.coordinates import celestial_to_cartesian, cartesian_to_celestial
 
 """
 A gentle introduction to decorators: https://towardsdatascience.com/how-to-use-decorators-in-python-by-example-b398328163b
@@ -36,22 +35,4 @@ def from_probit(func):
             return func(ref, *args)
         y = func(ref, *args)
         return transform_from_probit(y, ref.bounds)
-    return f_transf
-
-def cartesian(func):
-    """
-    Transform a point x from celestial coordinates to cartesian coordinates and returns the function evaluated at the probit point y
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = celestial_to_cartesian(x)
-        return func(ref, y, *args)
-    return f_transf
-
-def celestial(func):
-    """
-    Transform a point x from cartesian coordinates to celestial coordinates and returns the function evaluated at the probit point y
-    """
-    def f_transf(ref, x, *args, **kwargs):
-        y = cartesian_to_celestial(x)
-        return func(ref, y, *args)
     return f_transf
