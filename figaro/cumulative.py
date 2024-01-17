f3290906be42e5487ecee45225556b02fb95ce9a
sterinaldi <stefano.rinaldi@unipd.it>
2024-01-17 11:35:12 +0100

    reworked in python with numba

---
+++ figaro/cumulative.py
@@ -0,0 +1,46 @@
+import numpy as np
+from numba import njit, prange
+from figaro.mixture import _log1p_jit
+
+@njit
+def log_add(x, y):
+    if x >= y:
+        return x+_log1p_jit(np.exp(y-x))
+    else:
+        return y+_log1p_jit(np.exp(x-y))
+
+@njit
+def fast_log_cumulative(f):
+    """
+    Compute log cdf of probability density f
+    
+    Arguments:
+        :np.ndarray f: probability density
+    
+    Returns:
+        :np.ndarray: log cdf
+    """
+    n = f.shape[0]
+    h = np.zeros(n, dtype = np.double)
+    h[0] = f[0]
+    for i in prange(1,n):
+        h[i] = log_add(h[i-1],f[i])
+    return h
+
+@njit
+def fast_cumulative(f):
+    """
+    Compute cdf of probability density f
+    
+    Arguments:
+        :np.ndarray f: probability density
+    
+    Returns:
+        :np.ndarray: cdf
+    """
+    n = f.shape[0]
+    h = np.zeros(n, dtype = np.double)
+    h[0] = f[0]
+    for i in prange(1,n):
+        h[i] = h[i-1] + f[i]
+    return h
