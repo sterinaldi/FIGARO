8ba8b28697f88d1824ea7202046e3b7c4b485c55
sterinaldi <stefano.rinaldi@uni-heidelberg.de>
2025-01-25 13:18:49 +0100

    dropped lal

--- figaro/cosmology.py
+++ figaro/cosmology.py
@@ -1,13 +1,14 @@
 import numpy as np
-import warnings
-warnings.filterwarnings("ignore", "Wswiglal-redir-stdio") # Silence LAL warnings with ipython
-from scipy.optimize import newton
+#from scipy.optimize import newton
+from numba import njit
 from scipy.interpolate import interp1d
-from lal._lal import LuminosityDistance, ComovingTransverseDistance, ComovingLOSDistance, HubbleDistance, HubbleParameter, UniformComovingVolumeDensity, ComovingVolumeElement, ComovingVolume, CreateCosmologicalParameters
+from astropy.cosmology import wCDM, z_at_value
+from astropy.units import Quantity
+#from lal._lal import LuminosityDistance, ComovingTransverseDistance, ComovingLOSDistance, HubbleDistance, HubbleParameter, UniformComovingVolumeDensity, ComovingVolumeElement, ComovingVolume, CreateCosmologicalParameters
 
 class CosmologicalParameters:
     """
-    Wrapper for LAL functions in a single class.
+    Wrapper for Astropy functions in a single class.
     
     Arguments:
         double h:   normalised hubble constant h = H0/100 km/Mpc/s
@@ -28,79 +29,75 @@ class CosmologicalParameters:
         self.w0 = w0
         self.w1 = w1
         self.w2 = w2
-        self._CosmologicalParameters = CreateCosmologicalParameters(self.h, self.om, self.ol, self.w0, self.w1, self.w2)
-        self.HubbleDistance = HubbleDistance(self._CosmologicalParameters)
+        self.Cosmology = wCDM(H0 = self.h*100, Om0 = self.om, Ode0 = self.ol, w0 = self.w0)#, self.w1, self.w2)
+#        self.HubbleDistance = HubbleDistance(self._CosmologicalParameters)
         
-    def _vectorise(func):
-        def vectorised_func(self, x):
-            if hasattr(x, "__iter__"):
-                return np.array([func(self, xi) for xi in x])
-            else:
-                return func(self, x)
-        return vectorised_func
-    
-    @_vectorise
+#    def _vectorise(func):
+#        def vectorised_func(self, x):
+#            if hasattr(x, "__iter__"):
+#                return np.array([func(self, xi) for xi in x])
+#            else:
+#                return func(self, x)
+#        return vectorised_func
+#    
+#    @_vectorise
     def HubbleParameter(self, z):
-        return HubbleParameter(z, self._CosmologicalParameters)
+        return self.Cosmology.H(z).value
 
-    @_vectorise
+#    @_vectorise
     def LuminosityDistance(self, z):
-        return LuminosityDistance(self._CosmologicalParameters, z)
+        return self.cosmology.luminosity_distance(z).value
     
-    @_vectorise
+#    @_vectorise
     def ComovingTransverseDistance(self, z):
-        return ComovingTransverseDistance(self._CosmologicalParameters, z)
+        return self.Cosmology.comoving_transverse_distance(z).value
      
-    @_vectorise
+#    @_vectorise
     def ComovingLOSDistance(self, z):
-        return ComovingLOSDistance(self._CosmologicalParameters, z)
+        return self.Cosmology.comoving_distance(z).value
      
-    @_vectorise
-    def UniformComovingVolumeDensity(self, z):
-        return UniformComovingVolumeDensity(z, self._CosmologicalParameters)
+#    @_vectorise
+#    def UniformComovingVolumeDensity(self, z):
+#        return UniformComovingVolumeDensity(z, self._CosmologicalParameters)
     
-    @_vectorise
+    #@_vectorise
     def ComovingVolumeElement(self, z):
-        return ComovingVolumeElement(z, self._CosmologicalParameters)
+        return self.Cosmology.differential_comoving_volume(z).value
     
-    @_vectorise
+    #@_vectorise
     def ComovingVolume(self, z):
-        return ComovingVolume(self._CosmologicalParameters, z)
+        return self.Cosmology.comoving_volume(z).value
     
-    @_vectorise
-    def dDTdDC(self, DC):
-        if self.ok == 0.:
-            return 1.
-        elif self.ok > 0.:
-            return np.cosh(np.sqrt(self.ok)*DC/self.HubbleDistance)
-        else:
-            return np.cos(np.sqrt(-self.ok)*DC/self.HubbleDistance)
+#    @_vectorise
+#    def dDTdDC(self, DC):
+#        if self.ok == 0.:
+#            return 1.
+#        elif self.ok > 0.:
+#            return np.cosh(np.sqrt(self.ok)*DC/self.HubbleDistance)
+#        else:
+#            return np.cos(np.sqrt(-self.ok)*DC/self.HubbleDistance)
     
-    @_vectorise
-    def dDLdz(self, z):
-        DC   = self.ComovingLOSDistance(z)
-        DT   = self.ComovingTransverseDistance(z)
-        invE = self.HubbleParameter(z)
-        return DT + (1.+z)*self.dDTdDC(DC)*self.HubbleDistance*invE
+#    @_vectorise
+#    def dDLdz(self, z):
+#        DC   = self.ComovingLOSDistance(z)
+#        DT   = self.ComovingTransverseDistance(z)
+#        invE = self.HubbleParameter(z)
+#        return DT + (1.+z)*self.dDTdDC(DC)*self.HubbleDistance*invE
     
-    @_vectorise
+#    @_vectorise
     def Redshift(self, DL):
         if DL == 0.:
             return 0.
         else:
-            def objective(z, self, DL):
-                return DL - self.LuminosityDistance(z)
-            return newton(objective,1.0,args=(self, DL))
+            return z_at_value(self.Cosmology.luminosity_distance, Quantity(DL, 'Mpc'))
 
-Planck18 = CosmologicalParameters(0.674, 0.315, 0.685, -1, 0, 0)
-Planck15 = CosmologicalParameters(0.679, 0.3065, 0.6935, -1, 0, 0)
+Planck18 = CosmologicalParameters(0.674, 0.315, 0.685, -1)#, 0, 0)
+Planck15 = CosmologicalParameters(0.679, 0.3065, 0.6935, -1)#, 0, 0)
 
 # Interpolants up to z = 2.5
 z = np.linspace(0,2.5,1000)
 dvdz_planck18 = Planck18.ComovingVolumeElement(z)/1e9 # In Gpc
 dvdz_planck15 = Planck15.ComovingVolumeElement(z)/1e9 # In Gpc
-
-from numba import njit
 
 @njit
 def dVdz_approx_planck15(x):
